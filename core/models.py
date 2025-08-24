f96fce18070ee6b5aac31a6bafcd6a5dd14f3406
BekaMeskhoradze <beqa.mesxoradze97@gmail.com>
2025-08-24 12:43:35 +0400

    Add Views and Urls in core app

--- core/models.py
+++ core/models.py
@@ -17,10 +17,10 @@ class Size(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
-        return super().__str__()
+        return f"{self.name}"
 
-class Productsize(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='sizes')
+class ProductSize(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
 
