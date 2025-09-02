b421c9835139334372a971af9c82ccbae9b92d10
BekaMeskhoradze <beqa.mesxoradze97@gmail.com>
2025-09-02 20:29:13 +0400

    fix start script

--- koyeb_start.sh
+++ koyeb_start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+DJANGO_SETTINGS_MODULE=config.settings
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
